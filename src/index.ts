5e95086ac281d79c9ce5209101a5629ea2b40e38
zwkang <kang95630@gmail.com>
2024-06-05 16:47:02 +0800

    feat: support multiple settings

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Plugin, UserConfig, createFilter } from 'vite';
+import { Plugin, UserConfig, createFilter, type ResolvedConfig } from 'vite';
 
 import nodePath from 'node:path';
 import fs from 'node:fs';
@@ -9,9 +9,8 @@ export type ViteConfig = Readonly<
   Omit<UserConfig, 'plugins' | 'assetsInclude' | 'optimizeDeps' | 'worker'> & {
     include?: UserConfig['assetsInclude'];
     exclude?: UserConfig['assetsInclude'];
-    env?: string;
+    env?: string | string[];
     root?: string;
-    extension?: string[];
   }
 >;
 
@@ -22,30 +21,36 @@ const defaultOpts = {
   exclude: defaultExclude,
   include: [],
   root: process.cwd(),
-  extension: ['.overseas'],
 };
 
 export default function resolveEnhance(opt: ViteConfig = defaultOpts): Plugin {
   const config = { ...defaultOpts, ...opt };
+  let UserConfig: ResolvedConfig;
   return {
     name: 'vite-plugin-resolve-adapter',
     enforce: 'pre',
+    configResolved(resolvedConfig) {
+      UserConfig = resolvedConfig;
+    },
+
     async resolveId(id, importer, resolveOpts) {
       const filter = createFilter(config.include, config.exclude, {
-        resolve: config.root,
+        resolve: config.root || UserConfig.root,
       });
       const resolved = await this.resolve(id, importer, {
         skipSelf: true,
         ...resolveOpts,
       });
       if (resolved && filter(resolved.id)) {
-        const newPath = resolveExtensionPath(resolved.id, [config.env]);
+        const newPath = resolveExtensionPath(resolved.id, toArray(config.env));
         isDebug && console.log(`[adapter] ${id} -> ${newPath}`);
         return newPath;
       }
     },
   };
 }
+
+const maps = new Map();
 
 /**
  * get a path, according the extensions to detect file exists
@@ -54,16 +59,28 @@ export default function resolveEnhance(opt: ViteConfig = defaultOpts): Plugin {
  * @param extensions
  * @returns
  */
-function resolveExtensionPath(path: string, extensions: string[]) {
+function resolveExtensionPath(path: string, configEnv: string[]) {
   const cleanId = cleanUrl(path);
   const extname = nodePath.extname(cleanId);
   const filename = nodePath.basename(cleanId, extname);
   const dirname = nodePath.dirname(cleanId);
   if (!extname) return cleanId;
   const fullPath = nodePath.join(dirname, filename);
-  const ext = extensions.find((ext) => fs.existsSync(`${fullPath}.${ext}${extname}`));
+  const ext = configEnv.find((ext) => {
+    const newPath = `${fullPath}.${ext}${extname}`;
+    if (maps.has(newPath)) {
+      return maps.get(newPath);
+    }
+    const exists = fs.existsSync(newPath);
+    maps.set(newPath, exists);
+    return exists;
+  });
   if (ext) {
     return `${fullPath}.${ext}${extname}`;
   }
   return cleanId;
+}
+
+function toArray<T>(value: T | T[]): T[] {
+  return Array.isArray(value) ? value : [value];
 }
